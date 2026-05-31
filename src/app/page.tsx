b490ceb6826e08dae8022cc24d18f7c09f92f96e
mae134 <en.tps.luc@gmail.com>
2026-05-31 20:47:04 +0900

    init: create Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">
+        Home Page
+      </h1>
+
+      <p className="mt-4">
+        Next.jsのルーティングを学んでいます。
+      </p>
+    </main>
+  );
+}
