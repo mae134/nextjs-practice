8aa03b5afc29a9c235e23eab257de02584bd8213
mae134 <en.tps.luc@gmail.com>
2026-05-31 20:47:47 +0900

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">
+        About Page
+      </h1>
+
+      <p className="mt-4">
+        Next.jsのルーティングを学んでいます。
+      </p>
+    </main>
+  );
+}
