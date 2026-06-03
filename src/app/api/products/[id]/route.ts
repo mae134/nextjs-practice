676b770c2559d417657ed4cb4b2b1a3fab15214f
mae134 <en.tps.luc@gmail.com>
2026-06-03 23:57:05 +0900

    feat: add put and delete api routes

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -24,3 +24,34 @@ export async function GET(
 
   return Response.json(product);
 }
+
+export async function PUT(
+  request: Request,
+  { params }: {
+    params: Promise<{ id: string }>
+  }
+) {
+  const { id } = await params
+
+  const body = await request.json()
+
+  return Response.json({
+    message: "商品を更新しました",
+    id,
+    product: body,
+  })
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: {
+    params: Promise<{ id: string }>
+  }
+) {
+  const { id } = await params
+
+  return Response.json({
+    message: "商品を削除しました",
+    id,
+  })
+}
