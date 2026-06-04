672886c4b4fdd854e3a3d10546165f17c7a2e2ac
mae134 <en.tps.luc@gmail.com>
2026-06-04 19:35:57 +0900

    feat: implement product update and delete logic

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,4 +1,4 @@
-import { getProduct } from "@/data/products";
+import { getProduct, Product, updateProduct, deleteProduct } from "@/data/products";
 
 
 export async function GET(
@@ -35,10 +35,29 @@ export async function PUT(
 
   const body = await request.json()
 
+  const updatedProduct: Product = {
+    id: id,
+    name: body.name,
+    price: body.price,
+  }
+
+  const product = await updateProduct(id, updatedProduct)
+
+  if (!product) {
+    return Response.json(
+      {
+        message: "商品が見つかりません",
+      },
+      {
+        status: 404,
+      }
+    )
+  }
+
   return Response.json({
     message: "商品を更新しました",
     id,
-    product: body,
+    product: product,
   })
 }
 
@@ -49,6 +68,19 @@ export async function DELETE(
   }
 ) {
   const { id } = await params
+
+  const deletedProduct = await deleteProduct(id)
+
+  if (!deletedProduct) {
+    return Response.json(
+      {
+        message: "商品が見つかりません",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
 
   return Response.json({
     message: "商品を削除しました",
--- src/data/products.ts
+++ src/data/products.ts
@@ -1,3 +1,10 @@
+
+export type Product = {
+  id: string,
+  name: string,
+  price: number,
+}
+
 export const products = [
   { id: "1", name: "りんご", price: 120 },
   { id: "2", name: "みかん", price: 100 },
@@ -7,3 +14,30 @@ export const products = [
 export async function getProduct(id: string) {
   return products.find((product) => product.id === id)
 }
+
+export async function updateProduct(id: string, updatedProduct: Product) {
+  let product = await getProduct(id)
+
+  if(!product) {
+    return null
+  }
+
+  product.name = updatedProduct.name
+  product.price = updatedProduct.price
+
+  return product
+}
+
+export async function deleteProduct(id: string) {
+  const index = products.findIndex((p) => p.id === id)
+
+  if (index === -1) {
+    return null
+  }
+
+  const deletedProduct = products[index]
+
+  products.splice(index, 1)
+
+  return deletedProduct
+} 
