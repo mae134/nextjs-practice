bde4e5e17858926a0f86ca08bdaab120cdc6822a
mae134 <en.tps.luc@gmail.com>
2026-06-07 23:51:21 +0900

    feat: submit product form with fetch

--- src/app/products/client-form/page.tsx
+++ src/app/products/client-form/page.tsx
@@ -5,18 +5,38 @@ import { useState } from "react"
 export default function ClientFormPage() {
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = async () => {
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        price: Number(price),
+      }),
+    })
+
+    const data = await response.json()
+
+    setName("")
+    setPrice("")
+    setMessage(data.message)
+
+    console.log(data)
+  }
 
   return (
     <main className="p-8">
       <button
-        onClick={() => {
-          console.log(name)
-          console.log(price)
-        }}git
+        onClick={handleSubmit}
         className="border px-4 py-2"
       >
         テスト
       </button>
+
       <h1 className="text-3xl font-bold">Client Component Form</h1>
 
       <div className="mt-4 space-y-4">
@@ -45,6 +65,9 @@ export default function ClientFormPage() {
         <p>商品名: {name}</p>
         <p>価格: {price}</p>
       </div>
+      {message && (
+        <p className="text-green-600">{message}</p>
+      )}
     </main>
   )
 }
